dbcdfca2052cf0628ecc36990e93dfb10f4ec436
henon solomon <henon@henons-MacBook-Air.local>
2023-12-15 17:51:41 +0300

    Do models, and part of the application layer

---
+++ Application/Features/Contest/Handlers/Queries/GetOverallContestStandingRequestHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Application.Contracts;
+using Application.DTOs;
+using AutoMapper;
+using MediatR;
+
+namespace Application.Features.Contest.Handlers.Queries
+{
+	public class GetOverallContestStandingRequestHandler : IRequestHandler<GetOverallContestStandingRequest, List<UserContestHistoryDto>>
+	{
+        private readonly IUserContestHistoryRepository _userContestHistoryRepository;
+        private readonly IMapper _mapper;
+
+
+        public GetOverallContestStandingRequestHandler(IUserContestHistoryRepository userContestHistoryRepository, IMapper mapper)
+		{
+            _userContestHistoryRepository = userContestHistoryRepository;
+            _mapper = mapper;
+		}
+
+        public async Task<List<UserContestHistoryDto>> Handle(GetOverallContestStandingRequest request, CancellationToken cancellationToken)
+        {
+            await _userContestHistoryRepository.GetOverallContestStanding
+            return _mapper.Map<>();
+        }
+    }
+}
+
